83ed9866405068db937a48f4934a6fbb3bd7d2fd
Rick K <rick.the.southpaw@gmail.com>
2025-10-20 16:05:59 -0400

    lab work

--- src/Parsers/Parser.cs
+++ src/Parsers/Parser.cs
@@ -20,7 +20,7 @@ namespace Parser
         public static AST.BlockStmt Parse(string Program)
         {
             return null;
-            if (Program == null) { throw new ParseException("Program is null");}
+            if (Program == null) { throw new ParseException("Program is null"); }
             if (Program[0]._tkntype == TokenType.LEFT_CURLY && Program[Program.Count - 1]._tkntype == TokenType.RIGHT.CURLY)
             {
                 return AST.BlockStmt()
@@ -36,20 +36,20 @@ namespace Parser
         // helper function to parse the content of the expression
         private static AST.ExpressionNode ParseExpressionHelper(List<Tokenizer.Token> expression, int i)
         {
-                // if (expression[i]._tkntype == TokenType.LEFT_PAREN && expression[i+4]._tkntype == TokenType.RIGHT_PAREN)
-                if (expression[i]._tkntype == TokenType.LEFT_PAREN && i + 4 < expression.Count && expression[i+4]._tkntype == TokenType.RIGHT_PAREN)
-                {
-                    return ParseExpressionContent(expression.GetRange(i+1, 3));
-                }
+            // if (expression[i]._tkntype == TokenType.LEFT_PAREN && expression[i+4]._tkntype == TokenType.RIGHT_PAREN)
+            if (expression[i]._tkntype == TokenType.LEFT_PAREN && i + 4 < expression.Count && expression[i + 4]._tkntype == TokenType.RIGHT_PAREN)
+            {
+                return ParseExpressionContent(expression.GetRange(i + 1, 3));
+            }
 
-                return ParseExpressionHelper(expression.GetRange(i, expression.Count - i - 1), i+1);
+            return ParseExpressionHelper(expression.GetRange(i, expression.Count - i - 1), i + 1);
         }
 
         public static AST.ExpressionNode ParseExpressionContent(List<Tokenizer.Token> content)
         {
-            if (content.Count == 0) { throw new ParseException("No content");}
-            if (content.Count == 1) {return HandleSingleToken(content[0]);}
-            if(content.Count == 3)
+            if (content.Count == 0) { throw new ParseException("No content"); }
+            if (content.Count == 1) { return HandleSingleToken(content[0]); }
+            if (content.Count == 3) //handle x + y + z?
             {
                 var left = HandleSingleToken(content[0]);
 
@@ -65,34 +65,34 @@ namespace Parser
         public static AST.ExpressionNode HandleSingleToken(Tokenizer.Token token)
         {
             if (token._tkntype == TokenType.INTEGER) { return new LiteralNode(token._value); }
-            if (token._tkntype == TokenType.FLOAT) { return new LiteralNode(token._value);}
-            if (token._tkntype == TokenType.VARIABLE) { return new VariableNode(token._value);}
+            if (token._tkntype == TokenType.FLOAT) { return new LiteralNode(token._value); }
+            if (token._tkntype == TokenType.VARIABLE) { return new VariableNode(token._value); }
             throw new ParseException("Unexpected token may not not float or integer or variable");
         }
 
         public static AST.ExpressionNode CreateBinaryOperatorNode(string op, AST.ExpressionNode l, AST.ExpressionNode r)
         {
-            if (op == TokenConstants.PLUS) { return new AST.PlusNode(l,r);}
-            if (op == TokenConstants.MINUS) { return new AST.MinusNode(l,r); }
-            if (op == TokenConstants.TIMES) { return new AST.TimesNode(l,r); }
-            if (op == TokenConstants.INT_DIV) { return new AST.IntDivNode(l,r); } // This is a float division
-            if (op == TokenConstants.FLOAT_DIV) { return new AST.FloatDivNode(l,r); }
-            if (op == TokenConstants.MOD) { return new AST.ModulusNode(l,r); }
-            if (op == TokenConstants.EXP) { return new AST.ExponentiationNode(l,r); }
+            if (op == TokenConstants.PLUS) { return new AST.PlusNode(l, r); }
+            if (op == TokenConstants.MINUS) { return new AST.MinusNode(l, r); }
+            if (op == TokenConstants.TIMES) { return new AST.TimesNode(l, r); }
+            if (op == TokenConstants.INT_DIV) { return new AST.IntDivNode(l, r); } // This is a float division
+            if (op == TokenConstants.FLOAT_DIV) { return new AST.FloatDivNode(l, r); }
+            if (op == TokenConstants.MOD) { return new AST.ModulusNode(l, r); }
+            if (op == TokenConstants.EXP) { return new AST.ExponentiationNode(l, r); }
 
             throw new ParseException($"Invalid operator has been used: {op}");
         }
 
         public static AST.VariableNode ParseVariableNode(string variable)
         {
-            if (variable == null) { throw new ParseException("Variable is null");}
+            if (variable == null) { throw new ParseException("Variable is null"); }
             return new AST.VariableNode(variable);
         }
 
         // Individual Statements
-        public static AST.AssignmentStmt ParseAssignmentStmt(List<Tokenizer.Token> content, SymbolTable<string,object> keyval)
+        public static AST.AssignmentStmt ParseAssignmentStmt(List<Tokenizer.Token> content, SymbolTable<string, object> keyval)
         {
-            if (content.Count < 3)  throw new ParseException("Assignement statement al least need three tokens") ;
+            if (content.Count < 3) throw new ParseException("Assignement statement al least need three tokens");
 
             // check if the first token is a variable
             if (content[1]._tkntype == TokenType.ASSIGNMENT)
@@ -117,7 +117,7 @@ namespace Parser
 
         public static AST.Statement ParseStatement(List<Tokenizer.Token> content, SymbolTable<string, object> keyval)
         {
-            if (content[0]._tkntype == TokenType.RETURN){ return ParseReturnStatement(content); }
+            if (content[0]._tkntype == TokenType.RETURN) { return ParseReturnStatement(content); }
             if (content[0]._tkntype == TokenType.ASSIGNMENT)
             {
                 return ParseAssignmentStmt(content, keyval);
@@ -128,31 +128,33 @@ namespace Parser
         // Blocks
         public static void ParseStmtList(List<string> lines, BlockStmt stmt)
         {
-            var tknzier = new TokenizerImpl();
-            List<Tokenizer.Token> content = [];
-            foreach (string line in lines)
-            {
-                content.AddRange(tknzier.Tokenize(line));
-            }
+            //line by line
+            //check for left parenthasi in first posifition
 
-            if (lines.Count == 0) { throw new ParseException("Block stmt is empty");}
-            ParseStmtListHelper(content, stmt, 0);
+            //if it is, feed line by line to parse statment untill a parethasi
+            //if left parenthasi, call parseblockstatment
+
+            //if right parenthasi, escape recursion by passing back all info in a symbol table, and it will return if list<string> is empty
         }
 
-        public static void ParseStmtListHelper(List<Tokenizer.Token> content, BlockStmt stmt, int i)
-        {
-            if (content[i]._tkntype == TokenType.LEFT_CURLY && content[content.Count - 1]._tkntype == TokenType.RIGHT_CURLY)
-            {
-                ParseStatement(content.GetRange(i+1, content.Count - i - 1), stmt);
-            }
+        // public static void ParseStmtListHelper(List<Tokenizer.Token> content, BlockStmt stmt, int i)
+        // {
 
-            ParseStmtListHelper(content.GetRange(i, content.Count - i - 1), stmt, i+1);
-        }
+        // }
 
         public static AST.BlockStmt ParseBlockStmt(List<string> lines, SymbolTable<string, object> keyval)
         {
-            return new AST.BlockStmt(ParseStmtList(lines, keyval));
+            AST.BlockStmt Block = new BlockStmt([]);
+            var tknzier = new TokenizerImpl();
+            List<Tokenizer.Token> content = [];
+            content.AddRange(tknzier.Tokenize(lines[0]));
+            if (content[0]._tkntype == TokenType.LEFT_CURLY)
+            {
+                ParseStmtList(lines.GetRange(1, lines.Count - 1), Block);
+            }
 
+            return Block;
+            //Somehow check right parens for equality?
         }
     }
 }
--- src/Parsers/obj/Debug/net9.0/Parsers.AssemblyInfo.cs
+++ src/Parsers/obj/Debug/net9.0/Parsers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parsers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+561892c3ee43795cec584610c570cefef69ba6fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parsers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parsers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
