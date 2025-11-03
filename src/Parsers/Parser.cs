0d03f297e1c376106e996744ddfefeab0d3a2844
Rahul Ranjan Sah <rahulranjann45@gmail.com>
2025-11-02 23:47:30 -0500

    code w/ no comments

--- src/Parsers/Parser.cs
+++ src/Parsers/Parser.cs
@@ -19,19 +19,18 @@ namespace Parser
     }
     public static class Parser
     {
-        public static AST.BlockStmt Parse(string program)
+        public static AST.BlockStmt Parse(string Program) //Needs to be tokenized
         {
-            if (program == null) { throw new ParseException("Program is null"); }
-
-            var lines = program.Split("\n");
-
-            if (lines[0].StartsWith("{") && lines[lines.Length - 1].EndsWith("}"))
+            SymbolTable<string, object> blockScope = new SymbolTable<string, object>();
+            List<string> lines = new List<string>();
+            foreach (string line in Program.Split('\n'))
             {
-                var keyval = new SymbolTable<string, object>();
-                return ParseBlockStmt(lines, keyval);
+                lines.Add(line);
             }
-            throw new ArgumentException("Blocks braces are imbalanced");
+            if (lines.Count < 2) { throw new ParseException("Program must have atleas opening '{' and closing '}'."); }
+            return ParseBlockStmt(lines, blockScope);
         }
+
         // expressions from test it looks like: new[] { "(", "8", "/", "2", ")" }
         // non-recursive but innermost only
         public static AST.ExpressionNode ParseExpression(List<Token> expression)
@@ -41,43 +40,45 @@ namespace Parser
 
             if (expression[0]._tkntype == TokenType.LEFT_CURLY && expression[expression.Count - 1]._tkntype == TokenType.RIGHT_CURLY)
             {
-                throw new ParseException("mismatching curly braces");
+                throw new ParseException("mismatching parenthesis");
             }
 
-            return ParseExpressionContent(expression.GetRange(1, expression.Count - 1));
-
-            // return ParseExpressionHelper(expression, 0);
+            return ParseExpressionContent(expression.GetRange(1, expression.Count - 2));
         }
-
-        // // helper function to parse the content of the expression
-        // private static AST.ExpressionNode ParseExpressionHelper(List<Tokenizer.Token> expression, int i)
-        // {
-        //     // if (expression[i]._tkntype == TokenType.LEFT_PAREN && expression[i+4]._tkntype == TokenType.RIGHT_PAREN)
-        //     if (expression[i]._tkntype == TokenType.LEFT_PAREN && i + 4 < expression.Count && expression[i + 4]._tkntype == TokenType.RIGHT_PAREN)
-        //     {
-        //         return ParseExpressionContent(expression.GetRange(i + 1, 3));
-        //     }
-
-        //     return ParseExpressionHelper(expression.GetRange(i, expression.Count - i - 1), i + 1);
-        // }
 
         public static AST.ExpressionNode ParseExpressionContent(List<Tokenizer.Token> content)
         {
+            //keep track of a left and right and call cbn on them
             if (content.Count == 0) { throw new ParseException("No content"); }
+            if (content.Count == 1) { return HandleSingleToken(content[0]); } //This will handle things like 4
 
-            // base case
-            if (content.Count == 1) { return HandleSingleToken(content[0]); }
-            if (content.Count > 3) //handle x + y + z? more than two, ternary operator?
+            // we need head recursion call and then do stuff operator handling here
+            if (content[0]._tkntype == TokenType.LEFT_PAREN && content[content.Count - 1]._tkntype == TokenType.RIGHT_PAREN)
             {
-                var left = HandleSingleToken(content[0]);
-                var op = content[1];
-
-                var right = ParseExpressionContent(content.GetRange(2, content.Count - 1));
-
-                // intermediate stack return calls LIFO output
-                return CreateBinaryOperatorNode(op._value, left, right);
+                return ParseExpressionContent(content.GetRange(1, content.Count - 2));
             }
-            throw new ParseException("Invalid expression content");
+
+            for (int i = 0; i < content.Count; i++)
+            {
+                if (content[i]._tkntype == TokenType.LEFT_PAREN)
+                {
+                    // nested parenthesis will be handled later in the stack
+                    int parenCount = 1;
+                    i++;
+                    while (i < content.Count && parenCount > 0)
+                    {
+                        if (content[i]._tkntype == TokenType.LEFT_PAREN) { parenCount++; }
+                        else if (content[i]._tkntype == TokenType.RIGHT_PAREN) { parenCount--; }
+                        if (parenCount > 0) { i++; }
+                    }
+                }
+
+                if (content[i]._tkntype == TokenType.OPERATOR)   //if its an operator, left stuff is an expression node, right stuff is an expression node
+                {
+                    return CreateBinaryOperatorNode(content[i]._value, ParseExpressionContent(content.GetRange(0, i)), ParseExpressionContent(content.GetRange(i + 1, content.Count - i - 1))); //Return binaryopnode
+                }
+            }
+            throw new ParseException("Not a valid expression syntax");
         }
 
 
@@ -144,147 +145,82 @@ namespace Parser
             throw new ParseException("Invalid statement");
         }
 
-        //     //if right parenthasi, escape recursion by passing back all info in a symbol table, and it will return if list<string> is empty
+        //if right parenthasi, escape recursion by passing back all info in a symbol table, and it will return if list<string> is empty
         // }
-
         public static void ParseStmtList(List<string> lines, BlockStmt stmt)
         {
-            var tokenizer = new TokenizerImpl();
+            SymbolTable<string, object> Data = new SymbolTable<string, object>();
 
-            for (int i = 0; i < lines.Count; i++)
+            //line by line
+            var tknzier = new TokenizerImpl();
+            int i = 0;
+            while (i < lines.Count)
             {
                 string line = lines[i].Trim();
+                var content = tknzier.Tokenize(line);
 
-                // End of block
-                if (line == "}")
+                // Skip lines with no tokens (including empty lines)
+                if (content.Count == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                if (content[0]._tkntype == TokenType.LEFT_CURLY)
+                {
+                    // add everything Blockstmt handles it with peeling head recursion
+                    var block = ParseBlockStmt(lines.GetRange(i, lines.Count - i), Data);
+                    stmt.Statements.Add(block);
+
+                    // eat all the lines outer and recursion will take care of inner
+                    int curlyCount = 1;
+                    int lineBeingEaten = i + 1;
+                    while (lineBeingEaten < lines.Count && curlyCount > 0)
+                    {
+                        foreach (var token in tknzier.Tokenize(lines[lineBeingEaten]))
+                        {
+                            if (token._tkntype == TokenType.LEFT_CURLY) { curlyCount++; }
+                            else if (token._tkntype == TokenType.RIGHT_CURLY) { curlyCount--; }
+                        }
+                        lineBeingEaten++;
+                    }
+                    i += lineBeingEaten;
+
+                }
+                else if (content[0]._tkntype == TokenType.RIGHT_CURLY)
+                {
                     return;
-
-                var tokens = tokenizer.Tokenize(line);
-
-                // Handle nested block
-                if (tokens[0]._tkntype == TokenType.LEFT_CURLY)
-                {
-                    // The recursive call handles its own { ... } range
-                    var nestedBlock = ParseBlockStmt(lines.Skip(i).ToList(), new SymbolTable<string, object>());
-
-                    // Add nested block to current block's statements
-                    stmt.Statements.Add(nestedBlock);
-
-                    // Now skip the lines consumed by that block
-                    i += CountLinesInBlock(lines.Skip(i).ToList()) - 1;
                 }
                 else
                 {
-                    // Normal (non-block) statement
-                    stmt.Statements.Add(ParseStatement(tokens, new SymbolTable<string, object>()));
+                    var onelinerStmt = ParseStatement(content, Data);
+                    stmt.Statements.Add(onelinerStmt);
+                    i++;
                 }
             }
-
-            // If loop ends without }, it's a syntax error
-            throw new ParseException("Block not properly closed with '}'.");
         }
 
-
-        // public static void ParseStmtListHelper(List<Tokenizer.Token> content, BlockStmt stmt, int i)
-        // {
-
-        // }
-
-        // public static AST.BlockStmt ParseBlockStmt(List<string> lines, SymbolTable<string, object> keyval)
-        // {
-        //     AST.BlockStmt Block = new BlockStmt([]);
-        //     var tknzier = new TokenizerImpl();
-        //     List<Tokenizer.Token> content = [];
-        //     content.AddRange(tknzier.Tokenize(lines[0]));
-        //     if (content[0]._tkntype == TokenType.LEFT_CURLY)
-        //     {
-        //         ParseStmtList(lines.GetRange(1, lines.Count - 1), Block);
-        //     }
-
-        //     return Block;
-        //     //Somehow check right parens for equality?
-        // }
-
-        // add depth like BST to check if is balanced or not like a stack that tracks the numbers.
-        public static BlockStmt ParseBlockStmt(List<string> lines, SymbolTable<string, object> table)
+        public static AST.BlockStmt ParseBlockStmt(List<string> lines, SymbolTable<string, object> keyval)
         {
-            if (!lines[0].Trim().StartsWith("{"))
+            if (lines.Count == 0) { throw new ParseException("No strings"); }
+
+            var tknzier = new TokenizerImpl();
+            List<Tokenizer.Token> content = [];
+
+            content.AddRange(tknzier.Tokenize(lines[0]));
+            content.AddRange(tknzier.Tokenize(lines[lines.Count - 1]));
+
+            if (content[0]._tkntype != TokenType.LEFT_CURLY || content[1]._tkntype != TokenType.RIGHT_CURLY)
             {
-                throw new ParseException("BLock must start with {");
+                throw new ParseException("Block must start with '{' and end with '}'");
             }
 
-            //  put the list of statment into blocks
-            var stmt = new BlockStmt(new List<Statement>());
+            SymbolTable<string, object> blockScope = new SymbolTable<string, object>(keyval); //how to use symbol table here?
+            AST.BlockStmt Block = new BlockStmt([]);
 
-            for (int i = 1; i < lines.Count; i++)
-            {
-                // part 1: working with line trim each
-                string line = lines[i].Trim();
+            ParseStmtList(lines.GetRange(1, lines.Count - 1), Block);
 
-                // if hits break from loop out questionable placement
-                if (line == "}") break;
-
-                // tokenize each line instead of doing single lines
-                var tokens = new TokenizerImpl().Tokenize(line);
-
-                // does that happen early or late?
-                //  shouldn't recursion handle itself othercase if we do check for one case
-                if (tokens[0]._tkntype == TokenType.LEFT_CURLY)
-                {
-                    ParseStmtList(lines, stmt);
-                    var nestedBlock = ParseBlockStmt(ParseStmtList(lines, stmt), new SymbolTable<string, object>(table));
-
-                    // Blockstmt class has staments getter only do i need to make it to setter to?
-                    // Add that nested block to current block
-                    stmt.Statements.Add(innerBlock);
-
-                    // 🔹 Skip the lines we just consumed
-                    int consumed = CountLinesInBlock(remaining);
-                    i += consumed - 1; // -1 because loop will increment again
-                }
-                else
-                {
-                    // 🔹 Regular statement
-                    var parsedStmt = ParseStatement(tokens, symbols);
-                    stmt.Statements.Add(parsedStmt);
-                }
-            }
-
-            // If you finish the loop without a closing brace, it’s malformed
-            throw new ParseException("Block missing closing brace '}'.");
-        }
-
-                    stmt.Statements.Add(nestedBlock);
-
-                }
-
-            }
-
-            return stmt;
-
-            //     for (int i = 1; i < lines.Count; i++)
-            //     {
-            //         string line = lines[i].Trim();
-
-            //         if (line == "}") break; // end of this block
-
-            //         var tokens = new TokenizerImpl().Tokenize(line);
-
-            //         // Handle nested blocks recursively
-            //         if (tokens[0]._tkntype == TokenType.LEFT_CURLY)
-            //         {
-            //             var innerBlockLines = ExtractInnerBlock(lines, ref i);
-            //             var nestedBlock = ParseBlockStmt(innerBlockLines, new SymbolTable<string, object>(table));
-            //             block.Statements.Add(nestedBlock);
-            //         }
-            //         else
-            //         {
-            //             block.Statements.Add(ParseStatement(tokens, table));
-            //         }
-            //     }
-
-            //     return block;
-            // }
+            return Block;
 
         }
     }
