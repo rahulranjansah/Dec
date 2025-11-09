77575090d2add2327b4244552c729c42831ce313
Rahul Ranjan Sah <rahulranjann45@gmail.com>
2025-11-09 04:42:18 -0500

    added tests

--- src/Visitors/NameAnalysisVisitor.cs
+++ src/Visitors/NameAnalysisVisitor.cs
@@ -13,31 +13,31 @@ namespace AST
     }
 
     //  checks for naming correctness only so true or false
-    public class NameAnalysisVisitor :
-IVisitor<Tuple<SymbolTable<string, object>, Statement>, bool>
-    {
+//     public class NameAnalysisVisitor :
+// IVisitor<Tuple<SymbolTable<string, object>, Statement>, bool>
+//     {
 
-        private bool AnalyzeBinary(BinaryOperator node, Tuple<SymbolTable<string, object>, Statement> context)
-        {
-            bool leftOk = node.Left.Accept(this, context);
-            bool rightOk = node.Right.Accept(this, context);
-            return leftOk && rightOk;
-        }
+//         private bool AnalyzeBinary(BinaryOperator node, Tuple<SymbolTable<string, object>, Statement> context)
+//         {
+//             bool leftOk = node.Left.Accept(this, context);
+//             bool rightOk = node.Right.Accept(this, context);
+//             return leftOk && rightOk;
+//         }
 
-        public bool Visit(PlusNode node, Tuple<SymbolTable<string, object>, Statement> items)
-        {
-            // bool left = node.Left.Accept(this, items);
-            // bool right = node.Right.Accept(this, items);
-            // return left && right;
-            return AnalyzeBinary(node, items);
-        }
+//         public bool Visit(PlusNode node, Tuple<SymbolTable<string, object>, Statement> items)
+//         {
+//             // bool left = node.Left.Accept(this, items);
+//             // bool right = node.Right.Accept(this, items);
+//             // return left && right;
+//             return AnalyzeBinary(node, items);
+//         }
 
 
-        public bool Visit(MinusNode node, Tuple<SymbolTable<string,object>, Statement> items)
-        {
-            // return Visit(node, items);
-            return AnalyzeBinary(node, items);
-        }
-    }
+//         public bool Visit(MinusNode node, Tuple<SymbolTable<string,object>, Statement> items)
+//         {
+//             // return Visit(node, items);
+//             return AnalyzeBinary(node, items);
+//         }
+//     }
 
 }
